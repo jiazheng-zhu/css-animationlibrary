b519467e0817b9f4c9385099ecf511baa0ba9aec
jiazheng-zhu <73939587+jiazheng-zhu@users.noreply.github.com>
2021-04-22 23:02:16 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,40 @@
+function addD1(t) {
+    $('#element').removeClass()
+    $('#element').addClass('d1')
+}
+function addD2(t) {
+    $('#element').removeClass()
+    $('#element').addClass('d2')
+}
+function addD3(t) {
+    $('#element').removeClass()
+    $('#element').addClass('d3')
+}
+function addD4(t) {
+    $('#element').removeClass()
+    $('#element').addClass('d4')
+}
+function addD5(t) {
+    $('#element').removeClass()
+    $('#element').addClass('d5')
+}
+function addD6(t) {
+    $('#element').removeClass()
+    $('#element').addClass('d6')
+}
+function addD7(t) {
+    $('#element').removeClass()
+    $('#element').addClass('d7')
+}
+function addD8(t) {
+    $('#element').removeClass()
+    $('#element').addClass('d8')
+}
+function addD9(t) {
+    $('#element').removeClass()
+    $('#element').addClass('d9')
+}
+function addD10(t) {
+    $('#element').removeClass()
+    $('#element').addClass('d10')
+}
